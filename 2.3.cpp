a63815b9c4fc55d8153b10e2d57ec1f39b57b296
huanghao-code <huanghao_bupt@sina.com>
2017-06-11 00:15:15 +0800

    Create 2.3.cpp

---
+++ 2.3.cpp
@@ -0,0 +1,43 @@
+/**Ascending Order**/
+void MERGE(vector<int>& v, int p, int q, int r){
+    int i = 0;
+    int j = 0;
+    vector<int> lv(q - p + 1, 0);
+    vector<int> rv(r - q, 0);
+
+    for(j = 0, i = p; i <= q; i++, j++){
+        lv[j] = v[i];
+    }
+    for(j = 0, i = q + 1; i <= r; i++, j++){
+        rv[j] = v[i];
+    }
+
+    i = 0;
+    j = 0;
+    int k = p;
+    while(i < q - p + 1 && j < r - q){
+        if(lv[i] < rv[j]){
+            v[k] = lv[i];
+            i++;
+        }else{
+            v[k] = rv[j];
+            j++;
+        }
+        k++;
+    }
+    for(; i < q - p + 1; i++, k++){
+        v[k] = lv[i];
+    }
+    for(; j < r - q; j++, k++){
+        v[k] = rv[j];
+    }
+}
+
+void MERGE_SORT(vector<int>& v, int p, int r){
+    if(p < r){
+        int q = (p + r) / 2;
+        MERGE_SORT(v, p, q);
+        MERGE_SORT(v, q + 1, r);
+        MERGE(v, p, q, r);
+    }
+}
