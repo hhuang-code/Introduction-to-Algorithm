0ca57e4309516227fad9d81a5ea61b7c0d41115d
huanghao-code <huanghao_bupt@sina.com>
2017-06-24 00:29:40 +0800

    Create 5.1.cpp

---
+++ 5.1.cpp
@@ -0,0 +1,25 @@
+/**5.1-2**/
+int RANDOM_ZORE_ONE(){
+
+    Sleep(500);     //<windows.h>, to bring srand(time(0)) into effect
+    srand((unsigned)time(0));
+
+    return rand() % 2;
+}
+
+int RANDOM(int a, int b){
+    int k = 0;
+    k = ceil(log(b - a + 1) / log(2));
+
+    dynamic_bitset<> dbs(k);
+    do{
+        dbs.reset();             //set all bits to zore
+        for(int i = 0; i < k; i++){
+            if(RANDOM_ZORE_ONE() == 1){
+                dbs.set(i);      //set the bit at this pos to one
+            }
+        }
+    }while(dbs.to_ulong() >= b - a + 1);
+
+    return a + dbs.to_ulong();
+}
